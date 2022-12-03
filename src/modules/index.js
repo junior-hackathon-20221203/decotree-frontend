302c49d218fc228e53dff90f35aeb0c8d4a6b680
SunGun <65691192+gorjs3540@users.noreply.github.com>
2022-12-03 17:30:28 +0900

    Axios (#3)
    * add axios in package
    
    * implement getTree, postTree in modules
    
    Co-authored-by: SunGun <sungun@lionrocket.ai>

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,1 +1,23 @@
-// network
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://3.39.25.99',
+});
+
+export const getTree = async (id) => {
+  try {
+    const resp = await instance.get(`/tree/tree/${id}`);
+    return resp;
+  } catch (error) {
+    new Error(error);
+  }
+};
+
+export const postTree = async (id, payload) => {
+  try {
+    const resp = await instance.post(`/tree/tree/${id}`, { ...payload });
+    return resp;
+  } catch (error) {
+    new Error(error);
+  }
+};
