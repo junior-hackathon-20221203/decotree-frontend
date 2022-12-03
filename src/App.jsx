02adbf32bbb4a6f77d9e25e41e7f4328266acc6c
SunGun <65691192+gorjs3540@users.noreply.github.com>
2022-12-03 16:33:43 +0900

    Feature/hidden audio (#1)
    * add music files in assets
    
    * implement HiddenAudio in Components
    
    * add modules folder in src
    
    Co-authored-by: SunGun <sungun@lionrocket.ai>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import HiddenAudio from './components/HiddenAudio/HiddenAudio';
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<div>hi</div>} />
+        {/* test route -> tree 구현 후 병합 예정 */}
+        <Route path='/audio' element={<HiddenAudio />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/assets/music/index.js
@@ -0,0 +1,6 @@
+import carol1 from './carol1.mp3';
+import carol2 from './carol2.mp3';
+
+const CAROL_LIST = [carol1, carol2];
+
+export default CAROL_LIST;
