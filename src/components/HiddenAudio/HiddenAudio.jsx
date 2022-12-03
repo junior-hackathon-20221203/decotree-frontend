ce7d755cc3b9f55cd252d99e9cecd93de46d2feb
SunGun <sungun@lionrocket.ai>
2022-12-03 16:18:51 +0900

    implement HiddenAudio in Components

---
+++ src/components/HiddenAudio/HiddenAudio.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+import styled from 'styled-components';
+
+import { music } from '../../assets';
+
+const HiddenAudio = () => {
+  const audio = useRef(null);
+  const [loop, setLoop] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleMusicEnded = useCallback(() => {
+    setLoop((prev) => (prev === 3 ? 0 : ++prev));
+  }, []);
+
+  const handleAudio = () => {
+    setIsPlaying((prev) => !prev);
+    if (audio.current.paused) audio.current.play();
+    else audio.current.pause();
+  };
+
+  useEffect(() => {}, [loop, handleMusicEnded, audio]);
+
+  return (
+    <Wrapper>
+      <Audio
+        ref={audio}
+        src={music[loop]}
+        type='audio/mpeg'
+        onEnded={handleMusicEnded}
+      />
+      <Button onClick={handleAudio}>{!isPlaying ? 'Play' : 'Pause'}</Button>
+    </Wrapper>
+  );
+};
+
+export default HiddenAudio;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Audio = styled.audio`
+  width: 100px;
+  background-color: black;
+`;
+
+const Button = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  text-align: center;
+  line-height: 60px;
+  width: 100px;
+  height: 60px;
+  border: none;
+  font-weight: 500;
+  color: '#333';
+  cursor: pointer;
+`;
