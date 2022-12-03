20d07119a1c1387a092607450bcfcd62ea67d63a
SunGun <sungun@lionrocket.ai>
2022-12-03 17:15:39 +0900

    implement TreeTemplates in Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HiddenAudio from './components/HiddenAudio/HiddenAudio';
+import TreeTemplates from './components/Tree/TreeTemplates';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path='/' element={<div>hi</div>} />
         {/* test route -> tree 구현 후 병합 예정 */}
         <Route path='/audio' element={<HiddenAudio />} />
+        <Route path='/tree' element={<TreeTemplates />} />
       </Routes>
     </BrowserRouter>
   );
